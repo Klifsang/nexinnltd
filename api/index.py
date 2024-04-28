a2e6c796ab5c276aa4bfbd05939315573b860adf
Klif <sang.wicklife@student.moringaschool.com>
2024-04-28 13:08:17 +0300

    vercel

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
+# api/index.py
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'greetings':'Hello, World!'})
+    return jsonify({'greetings': 'Hello, World!'})
 
 @app.route('/about')
 def about():
-    return jsonify({'About'})
+    return jsonify({'message': 'About page'})
