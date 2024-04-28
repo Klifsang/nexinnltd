19d45a493ff9c6c58144f7499a2edfc168de9e42
Klif <sang.wicklife@student.moringaschool.com>
2024-04-28 13:37:16 +0300

    serving frontend

--- api/index.py
+++ api/index.py
@@ -1,11 +1,4 @@
-# api/index.py
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-
-
-
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__, static_folder='../client/dist', static_url_path='')
 
@@ -13,21 +6,6 @@ app = Flask(__name__, static_folder='../client/dist', static_url_path='')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/')
-# def home():
-#     return jsonify({'greetings': 'Hello, World!'})
-
-# @app.route('/about')
-# def about():
-#     return jsonify({'message': 'About page'})
+@app.route('/api/about')
+def about():
+    return jsonify({'message': 'About page'})
